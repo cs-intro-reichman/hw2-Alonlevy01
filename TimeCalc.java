f55eaf9704f66a9d523781162d0e124f900bb5bf
Alon Levy <alonlevy01@gmail.com>
2024-11-12 18:03:43 +0200

    first question upload

--- TimeCalc.java
+++ TimeCalc.java
@@ -1,5 +1,23 @@
 public class TimeCalc {
     public static void main(String[] args) {
-        // Replace this comment with your code
+        int hours = Integer.parseInt("" + args[0].charAt(0) + args[0].charAt(1));
+        int minutes = Integer.parseInt("" + args[0].charAt(3) + args[0].charAt(4));
+        int totalMinutes = hours*60 + minutes  + Integer.parseInt(args[1]);
+        int totalHours = totalMinutes/60;
+        int newHours = totalHours%24;
+        int newMinutes = totalMinutes - (totalHours*60);
+        if (newHours<10 && newMinutes<10) {
+            System.out.println("0"+ newHours + ":0" + newMinutes);
+        }
+        if (newHours<10 && newMinutes>9) {
+            System.out.println("0"+ newHours + ":" + newMinutes);
+        }
+        if (newHours>9 && newMinutes<10) {
+            System.out.println(newHours + ":0" + newMinutes);
+        }
+        if (newHours>9 && newMinutes>9) {
+            System.out.println(newHours + ":" + newMinutes);
+        }
+        }
     }
-}
+
